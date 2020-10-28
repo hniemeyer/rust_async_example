fd79b3a1f8e44ec0b5f0d2d4072a8342e0bc70a2
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2020-10-28 10:09:57 +0100

    Refactored random duration into own function

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ struct Message<T> {
     message: T
 }
 
+fn generate_random_amount_milliseconds(low: u64, high: u64) -> time::Duration
+{
+    let x = rand::thread_rng().gen_range(low, high);
+    time::Duration::from_millis(x)
+}
+
 #[tokio::main]
 async fn main() {
     let (mut tx, mut rx) = mpsc::channel(1);
@@ -15,18 +21,19 @@ async fn main() {
     tokio::spawn(async move {
 
         for idx in 0..10 {
-            let x = rand::thread_rng().gen_range(1, 100);
-            let amount = time::Duration::from_millis(x);
+            let amount = generate_random_amount_milliseconds(1, 100);
             thread::sleep(amount);
-            tx.send(Message  {task_id: 1, message: x}).await;
+            tx.send(Message  {task_id: 1, message: amount}).await;
         }
     });
 
     tokio::spawn(async move {
-        tx2.send(Message  {task_id: 2, message: 111}).await;
+        let amount = generate_random_amount_milliseconds(1, 20);
+        thread::sleep(amount);
+        tx2.send(Message  {task_id: 2, message: amount}).await;
     });
 
     while let Some(message) = rx.recv().await {
-        println!("GOT = {} from Task {}", message.message, message.task_id);
+        println!("GOT = {} from Task {}", message.message.as_millis(), message.task_id);
     }
 }
