c544308b5d8a9d01d111570efa949329dbbd58a5
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2020-10-28 10:16:05 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@ use tokio::sync::mpsc;
 
 struct Message<T> {
     task_id: u64,
-    message: T
+    message: T,
 }
 
-fn generate_random_amount_milliseconds(low: u64, high: u64) -> time::Duration
-{
+fn generate_random_amount_milliseconds(low: u64, high: u64) -> time::Duration {
     let x = rand::thread_rng().gen_range(low, high);
     time::Duration::from_millis(x)
 }
@@ -16,19 +15,25 @@ fn generate_random_amount_milliseconds(low: u64, high: u64) -> time::Duration
 #[tokio::main]
 async fn main() {
     let (mut tx, mut rx) = mpsc::channel(1);
-    
+
     for thread_num in 1..100 {
         let mut tx2 = tx.clone();
         tokio::spawn(async move {
             let amount = generate_random_amount_milliseconds(1, thread_num * 10);
             thread::sleep(amount);
-            tx2.send(Message  {task_id: thread_num, message: amount}).await;
+            tx2.send(Message {
+                task_id: thread_num,
+                message: amount,
+            })
+            .await;
         });
     }
 
-   
-
     while let Some(message) = rx.recv().await {
-        println!("GOT = {} from Task {}", message.message.as_millis(), message.task_id);
+        println!(
+            "GOT = {} from Task {}",
+            message.message.as_millis(),
+            message.task_id
+        );
     }
 }
